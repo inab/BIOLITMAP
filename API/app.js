7a83f2b830527de7b45c9d9985685b266a7373d7
Adrián Bazaga <adrianrodriguezbazaga@gmail.com>
2018-05-29 16:06:42 +0200

    Adding Biolitmap API REST source codes

---
+++ API/app.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var express = require('express');
+var indexRouter = require('./routes/index');
+var biolitmapRouter = require('./routes/biolitmap');
+var app = express();
+var createError = require('http-errors');
+
+app.use('/', indexRouter);
+app.use('/biolitmap', biolitmapRouter);
+// Catch 404 errors and forward them to the error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+module.exports = app;
+
+app.listen(3002);
---
+++ API/routes/biolitmap.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : '',
+    user: '',
+    password : '',
+    database: ''
+});
+
+connection.connect(function(error){
+  if(error){
+    console.log('Error');
+  }
+  else {
+    console.log('Connected!');
+  }
+});
+
+router.get('/', function(req, res, next) {
+  res.send('Root biolitmap')
+});
+
+router.get('/list', function(req, res, next) {
+  connection.query('SELECT * from biolitmap_data', function(err, rows, fields){
+    if (!err){
+        res.json(rows);
+    }
+    else{
+      console.log('Error while performing query');
+    }
+    });
+});
+
+router.get('/filter/nameAffiliation/:name', function(req, res, next) {
+  var name = req.params.name;
+  name = "'" + name + "'";
+  connection.query('SELECT * from biolitmap_data WHERE nameAffiliation = ' + name, function(err, rows, fields){
+    if (!err){
+        res.json(rows);
+    }
+    else{
+      console.log('Error while performing query');
+    }
+    });
+});
+
+router.get('/filter/DOI/:doi', function(req, res, next) {
+  var doi = req.params.doi;
+  doi = "'" + doi + "'";
+  connection.query('SELECT * from biolitmap_data WHERE DOI = ' + doi , function(err, rows, fields){
+    if (!err){
+        res.json(rows);
+    }
+    else{
+      console.log('Error while performing query');
+    }
+    });
+});
+
+router.get('/filter/year/:year', function(req, res, next) {
+  var year = req.params.year;
+  if (year[0]=='+'){
+    year= year.split("+")[1];
+    connection.query('SELECT * from biolitmap_data WHERE year >= ' + year, function(err, rows, fields){
+      if (!err){
+          res.json(rows);
+      }
+      else{
+        console.log('Error while performing query');
+      }
+      });  }
+  else if (year[0]=='-'){
+    year= year.split("-")[1];
+    connection.query('SELECT * from biolitmap_data WHERE year <= ' + year, function(err, rows, fields){
+      if (!err){
+          res.json(rows);
+      }
+      else{
+        console.log('Error while performing query');
+      }
+      });  }
+  else {
+    connection.query('SELECT * from biolitmap_data WHERE year = ' + year, function(err, rows, fields){
+      if (!err){
+          res.json(rows);
+      }
+      else{
+        console.log('Error while performing query');
+      }
+      });  }
+});
+
+router.get('/filter/source/:source', function(req, res, next) {
+  var source = req.params.source;
+  source = "'" + source + "'";
+  connection.query('SELECT * from biolitmap_data WHERE source = ' + source, function(err, rows, fields){
+    if (!err){
+        res.json(rows);
+    }
+    else{
+      console.log('Error while performing query');
+    }
+    });
+});
+
+router.get('/filter/edamCategory/:edamCategory', function(req, res, next) {
+  var edamCategory = req.params.edamCategory;
+  edamCategory = "'" + edamCategory + "'";
+  connection.query('SELECT * from biolitmap_data WHERE edamCategory = ' + edamCategory, function(err, rows, fields){
+    if (!err){
+        res.json(rows);
+    }
+    else{
+      console.log('Error while performing query');
+    }
+    });
+});
+
+module.exports = router;
